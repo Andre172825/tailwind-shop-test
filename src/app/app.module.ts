6ad8746b6b9c4da9feddadb8fc3cbc5184e88c23
Luis Tolentino <luist@4waystech.com>
2023-06-30 23:01:07 -0500

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/pages/header/header.component';
+import { FooterComponent } from './core/pages/footer/footer.component';
+import { MainComponent } from './core/pages/main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
