0cb5a5aa2f4497c7854cb96cbac077e96d23a25b
Luis Tolentino <luist@4waystech.com>
2023-07-02 17:05:57 -0500

    Actualización de body

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      responsive: ["responsive"],
+      colors: {
+        "gray-custom": "#ececec",
+        "light-blue-custom": "#7ec7fe",
+      },
+    },
   },
   plugins: [],
 };
