2a9e59a4597f8581a9247dddb3787b65a07aa089
Luis Tolentino <joseltolentino792@outlook.com>
2023-07-13 16:29:23 -0500

    routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './core/pages/main/main.component';
+import { MainV2Component } from './core/pages/main-v2/main-v2.component';
+import { MainV3Component } from './main-v3/main-v3.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'main-v2', component: MainV2Component },
+  { path: 'main-v3', component: MainV3Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
